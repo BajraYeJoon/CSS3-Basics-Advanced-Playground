8255f936eceaca44eacca5e3d2c3b8cdbeee2a67
BajraYeJoon <binaya.bajrashakya@gmail.com>
2022-03-14 13:48:55 +0545

    Code Clean

--- Box_Model/shared.js
+++ Box_Model/shared.js
@@ -2,21 +2,45 @@ var backdrop = document.querySelector(".backdrop");
 var planButtons = document.querySelectorAll(".plan button");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--no");
+var toggler = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 console.dir(planButtons);
 
 for (var i = 0; i < planButtons.length; i++) {
   planButtons[i].addEventListener("click", () => {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+
+    // modal.className = 'open';  this  will override the actual classes
+    backdrop.classList.add("open");
+    modal.classList.add("open");
   });
 }
 
 const closeModal = () => {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
+  // modal.style.display = "none";
+  // backdrop.style.display = "none";
+
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
 };
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", () => {
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
+});
 
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+toggler.addEventListener("click", () => {
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
